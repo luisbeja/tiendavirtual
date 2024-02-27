6649e90dc22b74b87abae09d114ec8798542a005
luisbeja <2017110739@udh.edu.pe>
2024-02-26 23:38:05 -0500

    first commit

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2020 Tienda Virtual - <a href="https://platea21.blogspot.com/"> https://platea21.blogspot.com</a> </b> Todos los derechos reservados.
+		</div>
+	</div>
